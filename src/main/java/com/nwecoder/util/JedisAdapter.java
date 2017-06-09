52e093bc1680754c6afafa1becb96d6a18737251
dengjiajun <807725057>
2017-06-09 23:41:06 +0800

    第六版本：用redis实现赞踩功能

--- src/main/java/com/nwecoder/util/JedisAdapter.java
+++ src/main/java/com/nwecoder/util/JedisAdapter.java
@@ -167,6 +167,7 @@ public class JedisAdapter implements InitializingBean {
     @Override
     public void afterPropertiesSet() throws Exception{
         pool = new JedisPool("redis://localhost:6379/10");
+
     }
 
 
